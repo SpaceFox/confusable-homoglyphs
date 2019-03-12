79d93c1fc621f4d0eb344f76026a303c33f7ac08
SpaceFox <ujueseo.yeou@gmail.com>
2019-03-13 00:17:44 +0100

    Fix: it was impossible to use internal data from JAR packaging

--- src/main/java/fr/spacefox/confusablehomoglyphs/UsesJsonData.java
+++ src/main/java/fr/spacefox/confusablehomoglyphs/UsesJsonData.java
@@ -3,9 +3,12 @@ package fr.spacefox.confusablehomoglyphs;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Type;
 import java.util.logging.Level;
@@ -13,7 +16,7 @@ import java.util.logging.Logger;
 
 /**
  * @author SpaceFox
- * @version 1.0.0
+ * @version 1.0.1
  */
 abstract class UsesJsonData<T> {
 
@@ -28,23 +31,33 @@ abstract class UsesJsonData<T> {
 
     boolean loadJson() {
         ClassLoader classLoader = getClass().getClassLoader();
-        String internalJsonFileName = classLoader.getResource(getInternalResourceName()).getFile();
-        return loadJson(internalJsonFileName);
+        InputStream resourceStream = classLoader.getResourceAsStream(getInternalResourceName());
+        return loadJson(resourceStream);
     }
 
     abstract String getInternalResourceName();
 
     boolean loadJson(String jsonFileName) {
-        try (Reader reader = new FileReader(jsonFileName)) {
-            setData(new GsonBuilder()
-                    .create()
-                    .fromJson(reader, realDataType));
-            initialized = true;
+        boolean initialized = false;
+        try (FileInputStream stream = new FileInputStream(new File(jsonFileName))) {
+            initialized = loadJson(stream);
         } catch (FileNotFoundException e) {
             LOGGER.severe("No " + jsonFileName + " data file found. Please regenerate it from CLI or download it.");
         } catch (IOException | JsonSyntaxException e) {
             LOGGER.log(Level.SEVERE, "Unable to load the " + jsonFileName + " data file due to " + e, e);
             LOGGER.severe("Please regenerate it from CLI or download it.");
+        }
+        return initialized;
+    }
+
+    private boolean loadJson(InputStream stream) {
+        try (Reader reader = new InputStreamReader(stream)) {
+            setData(new GsonBuilder()
+                    .create()
+                    .fromJson(reader, realDataType));
+            initialized = true;
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Unable to load data due to " + e, e);
         }
         return initialized;
     }
